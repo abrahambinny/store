5fb05342a5242e5aab17d6ededb7ca55b43d5f77
abrahambinny <binny.abraham@2connectbahrain.com>
2016-05-19 11:22:47 +0300

    Updated serializers for setting image urls

--- article/api_serializers.py
+++ article/api_serializers.py
@@ -1,19 +1,30 @@
 
 from rest_framework import serializers
 from .models import ContentDetail, Author, ImageStore, Category
+from django.conf import settings
 
 # Serializers define the API representation.
 class ImageStoreSerializer(serializers.HyperlinkedModelSerializer):
+
+    main_img = serializers.SerializerMethodField('get_main_img_value')
 
     class Meta:
         model = ImageStore
         fields = ('id', 'img_desc', 'main_img')
 
+    def get_main_img_value(self, obj):
+        return fetch_img_url(obj.main_img.name)
+
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+
+    author_avatar = serializers.SerializerMethodField('get_avatar')    
 
     class Meta:
         model = Author
         fields = ('id', 'author_name', 'author_desc', 'author_avatar')
+
+    def get_avatar(self, obj):
+        return fetch_img_url(obj.author_avatar.name)
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
 
@@ -30,4 +41,7 @@ class ContentDetailSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ContentDetail
         fields = ('id', 'title', 'pub_date', 'active', 'description', 'image', 'author', 'category')
+
+def fetch_img_url(img_path):
+    return settings.STATIC_URL+img_path.split(settings.STATIC_URL)[1]
 
