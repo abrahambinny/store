9378919c4048cf45ddf3e1e2be34611773b49237
abrahambinny <abrahambinny@gmail.com>
2016-06-02 15:34:28 +0300

    Committed change in settings for multiple domains

--- store/settings.py
+++ store/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +134,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
